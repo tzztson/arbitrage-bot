0e2f7aa954862649e4c3986c70ca94ff6dccfd72
tzztson <zhenghu61919@gmail.com>
2023-03-09 22:44:14 +0800

    feat: construct router for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+
 import './App.css';
+import { DarkProvider } from './context/darkThemeContext';
+
+import Home from './pages/home';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import Projects from './pages/projects';
+import HireMe from './pages/hireMe';
+import NotFound from './pages/notFound';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 export default function App() {
+
+  // const { darkMode, toggleDarkMode } = useContext(DarkContext);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <DarkProvider>
+        <Router>
+          <Routes>
+            {/* <Route
+              path="/"
+              element={<Navigate to={"/home"} />}
+            /> */}
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/hireme" element={<HireMe />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </DarkProvider>
+    </div>
   )
 }
---
+++ src/pages/hireMe.js
@@ -0,0 +1,9 @@
+const HireMe = () => {
+    return (
+        <div>
+            <p>Hire Me</p>
+        </div>
+    )
+}
+
+export default HireMe;
